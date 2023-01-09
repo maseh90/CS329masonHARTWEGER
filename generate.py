2b2d66611c77991e2dd45093b5ddb46e3441c251
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-09 12:42:42 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -12,5 +12,5 @@ for i in range(5):
   for ii in range(5):
     for iii in range(10):
       name_new = name_new + str(index)
-      pyrosim.Send_Cube(name=name_new), pos=[x+i,y+ii,z+iii] , size=[length*0.9**iii,width*0.9**iii,height*0.9**iii])
+      pyrosim.Send_Cube(name=name_new, pos=[x+i,y+ii,z+iii], size=[length*0.9**iii,width*0.9**iii,height*0.9**iii])
 pyrosim.End()
