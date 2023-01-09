629ce3359eff50934cab472c92c60cee95f3b8cc
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-09 13:07:55 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,11 +1,12 @@
 import pyrosim.pyrosim as pyrosim
 def Create_World():
   pyrosim.Start_SDF("world.sdf")
-  pyrosim.Send_Cube(name="Box1", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.Send_Cube(name="Box1", pos=[3,3,3], size=[1,1,1])
   pyrosim.End()
 def Create_Robot():
   pyrosim.Start_URDF("body.urdf")
   pyrosim.Send_Cube(name="TORSO", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.Send_Cube(name="LEG", pos=[1,0,1.5], size=[1,1,1])
   pyrosim.End()
 length = 1
 width = 1
