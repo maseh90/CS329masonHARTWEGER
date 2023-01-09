c7e0bcf86f28cedd962b1d9c0aea8ea074ca6353
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-09 13:02:51 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -3,6 +3,10 @@ def Create_World():
   pyrosim.Start_SDF("world.sdf")
   pyrosim.Send_Cube(name="Box1", pos=[0,0,0.5], size=[1,1,1])
   pyrosim.End()
+def Create_Robot():
+  pyrosim.Start_URDF("body.urdf")
+  pyrosim.Send_Cube(name="TORSO", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.End()
 length = 1
 width = 1
 height = 1
@@ -11,4 +15,4 @@ y = 0
 z = 0.5
 index = 0
 Create_World()
-
+Create_Robot()
