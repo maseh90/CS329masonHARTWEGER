689a2dd01414da7b4000b8acdf2a639b93c4a1ae
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-09 13:35:59 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -9,7 +9,7 @@ def Create_Robot():
   pyrosim.Send_Joint(name = "FrontLeg_Torso" , parent= "FrontLeg" , child = "Torso" , type = "revolute", position = [0.5,0,1])
   pyrosim.Send_Cube(name="Torso", pos=[0.5,0,0.5], size=[1,1,1])
   pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,0])
-  pyrosim.Send_Cube(name="BackLeg", pos=[0,0,-0.5], size=[1,1,1])
+  pyrosim.Send_Cube(name="BackLeg", pos=[1,0,-0.5], size=[1,1,1])
   pyrosim.End()
 length = 1
 width = 1
