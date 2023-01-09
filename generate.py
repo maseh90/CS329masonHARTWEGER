8b181d75c576a7c2fe05bb651e51b4739c945061
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-09 13:32:58 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -5,9 +5,11 @@ def Create_World():
   pyrosim.End()
 def Create_Robot():
   pyrosim.Start_URDF("body.urdf")
-  pyrosim.Send_Cube(name="Link0", pos=[0,0,0.5], size=[1,1,1])
-  pyrosim.Send_Joint(name = "Link0_Link1" , parent= "Link0" , child = "Link1" , type = "revolute", position = [0,0,1])
-  pyrosim.Send_Cube(name="Link1", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.Send_Cube(name="FrontLeg", pos=[0,0,0.5], size=[1,1,1])
+  pyrosim.Send_Joint(name = "FrontLeg_Torso" , parent= "FrontLeg" , child = "Torso" , type = "revolute", position = [0.5,0,1])
+  pyrosim.Send_Cube(name="Torso", pos=[0.5,0,0.5], size=[1,1,1])
+  pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0.5,0,-0.5])
+  pyrosim.Send_Cube(name="BackLeg", pos=[0.5,0,-0.5], size=[1,1,1])
   pyrosim.End()
 length = 1
 width = 1
