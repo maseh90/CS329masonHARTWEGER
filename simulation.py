5a511392735e2b66455b6785fe63964c4c05d92b
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 11:11:05 -0600

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -3,3 +3,6 @@ class SIMULATION:
     simulation = SIMULATION()
     self.world = WORLD()
     self.robot = ROBOT()
+    self.physicsClient = p.connect(p.GUI)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.setGravity(0,0,-9.8)
