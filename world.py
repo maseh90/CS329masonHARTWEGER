299d5dc1dc28ac47d68866dbf2a313f27ee0a9ca
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 11:08:09 -0600

    Update world.py

--- world.py
+++ world.py
@@ -1,9 +1,3 @@
 class WORLD:
   def __init__(self):
     world = WORLD()
-  def Prepare_To_Simulate(self):
-    physicsClient = p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())
-    p.setGravity(0,0,-9.8)
-    planeId = p.loadURDF("plane.urdf")
-    p.loadSDF("world.sdf")
