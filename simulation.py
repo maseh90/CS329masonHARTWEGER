fcd0964de8dd97431e060d1db2660490f9af66e8
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 11:26:54 -0600

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -10,8 +10,8 @@ from world import WORLD
 from robot import ROBOT
 class SIMULATION:
   def __init__(self):
+    self.physicsClient = p.connect(p.GUI)
     self.world = WORLD()
     self.robot = ROBOT()
-    self.physicsClient = p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,-9.8)
