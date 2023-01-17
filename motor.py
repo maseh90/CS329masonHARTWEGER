1d784e3a8ed45cd967a4397ef524fd9f38b3bd8e
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 12:59:36 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,4 +1,7 @@
 import pyrosim.pyrosim as pyrosim
 class MOTOR:
-  def __init__(self):
-    self.jointName = jointName
+  def __init__(self,jointNameInput):
+    self.jointName = jointNameInput
+    self.Prepare_to_Act()
+  def Prepare_to_Act(self):
+    pass
