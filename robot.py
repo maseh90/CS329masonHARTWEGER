4114dd01b900704a61cb03065fa803c6e721c33d
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 11:25:24 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,4 +1,14 @@
+import pybullet as p
+import pybullet_data
+import time
+import pyrosim.pyrosim as pyrosim
+import constants as c
+import numpy
+import random
+import math
 class ROBOT:
   def __init__(self):
     self.sensors = {}
     self.motors = {}
+    self.robotId = p.loadURDF("body.urdf")
+    pyrosim.Prepare_To_Simulate(robotId)
