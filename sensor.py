c97f295197f3ea2d9f2576c29d38a306adc7cf1a
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 12:34:31 -0600

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -2,3 +2,5 @@ class SENSOR:
   def __init__(self,inputName,data):
     self.linkName = inputName
     self.values = data
+  def Get_Value(self):
+    return pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
