725d6ef77ec02f7ae37e7a1b3993ed9afba8b2b4
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 11:24:19 -0600

    Update world.py

--- world.py
+++ world.py
@@ -1,3 +1,12 @@
+import pybullet as p
+import pybullet_data
+import time
+import pyrosim.pyrosim as pyrosim
+import constants as c
+import numpy
+import random
+import math
 class WORLD:
   def __init__(self):
-    pass
+    self.planeId = p.loadURDF("plane.urdf")
+    p.loadSDF("world.sdf")
