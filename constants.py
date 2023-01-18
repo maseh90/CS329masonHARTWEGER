3ffcb5b4b7846b193271d527c12d8d5724dbcaaf
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 19:18:40 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,6 +2,10 @@ import math
 amplitude = math.pi/2
 frequency = 1
 offset = 0
+sleepTime = 1/60
+simulationSteps = 1000
+maxForce = 150
+gravity = -9.8
 #amplitudeFront = math.pi/2
 #frequencyFront = 1
 #phaseOffsetFront = 0.5*math.pi
