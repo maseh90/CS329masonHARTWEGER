036436c0ed4df1b66ec5e833f0b97c1a5e763f91
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 18:38:28 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 import math
-amplitudeBack = math.pi/2
-frequencyBack = 1
-phaseOffsetBack = 0
-amplitudeFront = math.pi/2
-frequencyFront = 1
-phaseOffsetFront = 0.5*math.pi
+amplitude = math.pi/2
+frequency = 1
+offset = 0
+#amplitudeFront = math.pi/2
+#frequencyFront = 1
+#phaseOffsetFront = 0.5*math.pi
