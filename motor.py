a4676c7afff856dc0aa10c74a0945323d3b62977
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 18:37:58 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -1,7 +1,10 @@
 import pyrosim.pyrosim as pyrosim
+import constants as c
 class MOTOR:
   def __init__(self,jointNameInput):
     self.jointName = jointNameInput
     self.Prepare_to_Act()
   def Prepare_to_Act(self):
-    pass
+    self.amplitude = c.amplitude
+    self.frequency = c.frequency
+    self.offset = c.offset
