aebe01080f86e424433dfdbe763324fa37705cea
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-17 19:22:48 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -11,7 +11,6 @@ from motor import MOTOR
 class ROBOT:
   def __init__(self):
     self.robotId = p.loadURDF("body.urdf")
-    self.numberTimeSteps = timeSteps
     pyrosim.Prepare_To_Simulate(self.robotId)
     self.Prepare_To_Sense()
     self.Prepare_To_Act()
