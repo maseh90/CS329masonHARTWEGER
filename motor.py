4fbdac4585c56e95ad7436c8375a485dfb7ec7d3
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-20 17:48:01 -0600

    Update motor.py

--- motor.py
+++ motor.py
@@ -6,15 +6,6 @@ class MOTOR:
   def __init__(self,jointNameInput):
     self.jointName = jointNameInput
     self.Prepare_to_Act()
-  def Prepare_to_Act(self):
-    self.amplitude = c.amplitude
-    self.frequency = c.frequency
-    self.offset = c.offset
-    self.valueTimes = numpy.arange(0,c.simulationSteps,1)
-    if self.jointName == "Torso_BackLeg":
-      self.motorValues = self.amplitude * numpy.sin(self.frequency/2*self.valueTimes + self.offset)
-    if self.jointName == "Torso_FrontLeg":
-      self.motorValues = self.amplitude * numpy.sin(self.frequency*self.valueTimes + self.offset)
   def Act(self):
     self.Set_Value()
   def Set_Value(self,robotIdInput,desiredAngle):
