475c96750fdf826fd759237ea55f88ac573a88e8
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-20 16:22:45 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -11,6 +11,18 @@ def Create_Robot():
   pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
   pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
   pyrosim.End()
+def Generate_Body():
+  pyrosim.Start_URDF("body.urdf")
+  pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
+  pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+  pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[1,1,1])
+  pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+  pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
+  pyrosim.End()
+def Generate_Brain():
+  pyrosim.Start_NeuralNetwork("brain.nndf")
+  pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
+  pyrosim.End()
 length = 1
 width = 1
 height = 1
@@ -18,5 +30,5 @@ x = 0
 y = 0
 z = 0.5
 index = 0
-Create_World()
-Create_Robot()
+Generate_Body()
+Generate_Brain()
