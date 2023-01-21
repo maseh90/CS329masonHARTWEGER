1a0f01dfd6140ccdb8bc5e6ed1d8be63daa50506
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-21 15:52:46 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -28,8 +28,8 @@ def Generate_Brain():
   pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
   pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = 0 )
   pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = 0 )
-  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1.0 )
-  pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1.0 )
+  pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 0 )
+  pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 0 )
   pyrosim.End()
 length = 1
 width = 1
