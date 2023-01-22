56ef57336b68fcb7f871c95871859e1e3f065b8f
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-21 18:15:02 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import pyrosim.pyrosim as pyrosim
 from sensor import SENSOR
 from motor import MOTOR
 from pyrosim.neuralNetwork import NEURAL_NETWORK
-from pyrosim.neurons import NEURONS
+from pyrosim.neuron import NEURON
 def Create_World():
   pyrosim.Start_SDF("world.sdf")
   pyrosim.Send_Cube(name="Box1", pos=[3,3,3], size=[1,1,1])
