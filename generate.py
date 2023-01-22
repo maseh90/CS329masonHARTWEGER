2c6968522853c6266d6fbd515acde2195ff983f9
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-21 18:03:18 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -34,11 +34,11 @@ def Generate_Brain():
   pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1 )
   pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1 )
   #for neuronName in self.nn.Get_Neuron_Names():
-  for key_neuron in self.neurons:
-      if self.neurons[key_neuron].Is_Sensor_Neuron():
-          self.neurons[key_neuron].Update_Sensor_Neuron()
-      for key_neuron in self.neurons:
-        if self.neurons[key_neuron].Is_Sensor_Neuron():
+  for key_neuron_sensor in self.neurons:
+      if self.neurons[key_neuron_sensor].Is_Sensor_Neuron():
+        for key_neuron_motor in self.neurons:
+          if self.neurons[key_neuron_motor].Is_Motor_Neuron():
+            pyrosim.Send_Synapse( sourceNeuronName = self.neurons[key_neuron_sensor] , targetNeuronName = self.neurons[key_neuron_motor] , weight = 1 )
   pyrosim.End()
 length = 1
 width = 1
