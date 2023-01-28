5afa98dd179f35d47ca74a407dc39e09b7069f5a
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-27 20:23:46 -0600

    Update hillclimber.py

--- hillclimber.py
+++ hillclimber.py
@@ -1,9 +1,23 @@
 import pyrosim.pyrosim as pyrosim
-
+import constants as c
 from solution import SOLUTION
 class HILL_CLIMBER:
   def __init__(self):
     self.parent = SOLUTION()
     self.parent.Evaluate()
   def Evolve(self):
+    for currentGeneration in range(c.numberOfGenerations):
+      self.Evolve_For_One_Generation()
+  def Evolve_For_One_Generation(self):
+    self.Spawn()
+    self.Mutate()
+    self.child.Evaluate()
+    self.Select()
+  def Spawn(self):
+    pass
+  def Mutate(self):
+    pass
+  def Evaluate(self):
+    pass
+  def Select(self):
     pass
