887c6d48240d86d0bcc6c55ab629350ffc70ed9b
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-30 00:23:58 -0600

    Update parallelHillClimber.py

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -49,9 +49,9 @@ class PARALLEL_HILL_CLIMBER:
     #exit()
   def Evaluate(self,solutions):
     for key_parent in solutions:
-      solutions[key_parent].Start_Simulation("GUI")
+      solutions[key].Start_Simulation("GUI")
     for key_parent in solutions:
-      solutions[key_parent].Wait_For_Simulation_To_End()
+      solutions[key].Wait_For_Simulation_To_End()
   def Select(self):
     if (self.parent.fitness > self.child.fitness):
       self.parent = self.child
