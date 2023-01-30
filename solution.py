3a47f020a9112b0aad104718467ec5a5e7bdc979
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-01-30 10:23:19 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -35,11 +35,12 @@ class SOLUTION:
   def Create_Body(self):
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0,0,1], size=[1,1,1])
-    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
-    pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[1,1,1])
-    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0.5,1])
+    pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [0,-0.5,1], jointAxis = "0 1 0")
+    pyrosim.Send_Cube(name="BackLeg", pos=[0,-0.5,0], size=[0.2,1,0.2])
+    pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [0,0.5,1], jointAxis = "0 1 0")
     pyrosim.Send_Cube(name="FrontLeg", pos=[0,0.5,0], size=[0.2,1,0.2])
     pyrosim.End()
+    exit()
   def Create_Brain(self):
     brain_file = "brain" + str(self.myID) + ".nndf"
     pyrosim.Start_NeuralNetwork(brain_file)
