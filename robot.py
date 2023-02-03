7044027dcf923a96fc914f3d39aea80f2f99a6dc
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-02 20:27:45 -0600

    Update robot.py

--- robot.py
+++ robot.py
@@ -15,8 +15,9 @@ class ROBOT:
     brain_name = "brain" + str(solutionID) + ".nndf"
     self.solutionID = solutionID
     self.robotId = p.loadURDF("body.urdf")
-    pyrosim.Prepare_To_Simulate(self.robotId)
     self.nn = NEURAL_NETWORK(brain_name)
+    pyrosim.Prepare_To_Simulate(self.robotId)
+
     self.Prepare_To_Sense()
     self.Prepare_To_Act()
     os.system("rm "+brain_name)
