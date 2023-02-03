e884b81ff0360f61718efd2b6e8e1398a6d7ab6a
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-02 19:44:09 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -30,8 +30,8 @@ class SOLUTION:
     #print(self.fitness)
   def Create_World(self):
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box1", pos=[3,3,3], size=[100,100,1])
-    pyrosim.Send_Cube(name="FirstStep", pos=[-2,2,3], size=[1,1,1])
+    pyrosim.Send_Cube(name="Box1", pos=[3,3,3], size=[1,1,1])
+    pyrosim.Send_Cube(name="FirstStep", pos=[-2,2,3], size=[100,100,1])
     pyrosim.End()
   def Create_Body(self):
     pyrosim.Start_URDF("body.urdf")
