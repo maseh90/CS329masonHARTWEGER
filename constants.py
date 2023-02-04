643856960a2ac30c26a80a751bcc15da5e310451
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-03 20:55:51 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 import math
-amplitude = math.pi
+amplitude = 1.5*math.pi/2
 frequency = 0.25
 offset = 0
 sleepTime = 1/120
 simulationSteps = 1000
 maxForce = 80
 gravity = -9.8
-numberOfGenerations = 2
-populationSize = 2
+numberOfGenerations = 5
+populationSize = 5
 numSensorNeurons = 8
 numMotorNeurons = 16
-motorJointRange = 0.75
+motorJointRange = 0.4
