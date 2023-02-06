f619de57ec51361d539e72c73b6c43f320dc475c
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-06 01:05:24 -0600

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,10 +1,10 @@
 import math
-amplitude = 2*math.pi
+amplitude = 1.5*math.pi/2
 frequency = 2
 offset = 0
 sleepTime = 1/60
-simulationSteps = 7500
-maxForce = 270
+simulationSteps = 12500
+maxForce = 300
 gravity = -9.8
 numberOfGenerations = 25
 populationSize = 10
