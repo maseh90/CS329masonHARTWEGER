14b70de6fec908d88bf9f9273cf35bd030cb8bc9
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-13 12:42:46 -0600

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -14,6 +14,7 @@ class SIMULATION:
       self.physicsClient = p.connect(p.DIRECT)
     if directOrGUI == "GUI":
       self.physicsClient = p.connect(p.GUI)
+      p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0,0,c.gravity)
     self.world = WORLD()
