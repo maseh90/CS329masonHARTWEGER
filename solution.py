e699b6a2e4c990d0c3aa0d23d896b445c5ba2090
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-13 14:40:53 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -77,7 +77,7 @@ class SOLUTION:
     joint_name_list = []
     for i in range(number_body_elements-1):
       pyrosim.Send_Cube(name=names_body_elements[i], pos=[body_element_x[i],body_element_y[i],body_element_z[i]], size=[body_element_width[i],body_element_length[i],body_element_height[i]])
-      if (i == (number_body_elements - 1)):
+      if (i == (number_body_elements - 2)):
         break
       name_new = names_body_elements[i] + "_" + names_body_elements[i+1]
       joint_name_list.append(name_new)
