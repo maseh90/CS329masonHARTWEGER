ae3c447095f54291a42f6344301c66c7645a87a2
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-13 14:08:21 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -65,8 +65,9 @@ class SOLUTION:
       names_body_elements[i] = str(i)
     body_element_x[0] = 0
     body_element_z[0] = 1.5
-    joint_element_x[0] = 0 + body_element_width[0]/2
-    joint_element_z[0] = 1.5
+    if number_body_elements != 1:
+      joint_element_x[0] = 0 + body_element_width[0]/2
+      joint_element_z[0] = 1.5
     for i in range(number_body_elements-1):
       body_element_x[i+1] = joint_element_x[0] + body_element_width[i+1]/2
       if number_body_elements == 1:
