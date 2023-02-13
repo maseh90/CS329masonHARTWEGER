5b0e804acf3f4f12a9502c2693b39cd54a3cf450
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-13 13:49:28 -0600

    Update search.py

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ from parallelHillClimber import PARALLEL_HILL_CLIMBER
 
 phc = PARALLEL_HILL_CLIMBER()
 #phc.Show_Best()
-#phc.Evolve()
+phc.Evolve()
 phc.Show_Best()
 #for i in range(5):
 #  os.system("python3 generate.py")
