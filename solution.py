a6756e7dc61d1b86cfacd56d3f121e4863d17d6b
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-13 15:17:09 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -121,7 +121,7 @@ class SOLUTION:
     print(self.numMotor_Neurons)
     for currentRow in sensor_neurons:
       for currentColumn in motor_neurons:
-        pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn, weight = self.weights[currentRow-1][currentColumn-sensor_name_index-2] )
+        pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn, weight = self.weights[currentRow-1][currentColumn-sensor_name_index-1] )
     pyrosim.End()
     #exit()
   def Mutate(self):
