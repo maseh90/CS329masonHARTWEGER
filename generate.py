8a22d68ec75bf4fab6f75dce1a6c45e4ab0abf7f
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-13 13:16:49 -0600

    Update generate.py

--- generate.py
+++ generate.py
@@ -16,15 +16,49 @@ def Create_Robot():
   pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
   pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
   pyrosim.End()
-def Generate_Body():
+def Generate_Body_and_Brain():
+  number_body_elements = random.randint(1,10)
+  names_body_elements = ["0"]*number_body_elements
+  body_element_width = [0]*number_body_elements
+  body_element_length = [0]*number_body_elements
+  body_element_height = [0]*number_body_elements
+  body_element_x = [0]*number_body_elements # this will change, build in +x directiomn
+  body_element_y = [0]*number_body_elements
+  body_element_z = [0]*number_body_elements
+  joint_element_x = [0]*(number_body_elements-1) # this will change, build in +x directiomn
+  joint_element_y = [0]*(number_body_elements-1)
+  joint_element_z = [0]*(number_body_elements-1)
+  touch_sensor_no_sensor = [0]*number_body_elements
+  for i in range(number_body_elements):
+    body_element_width[i] = round(random.uniform(0,3),2)
+    body_element_length[i] = round(random.uniform(0,3),2)
+    body_element_height[i] = round(random.uniform(0,3),2)
+    touch_sensor_no_sensor[i] = random.randint(0,1)
+    names_body_elements[i] = str(i)
+  body_element_x[0] = 0
+  body_element_z[0] = 1.5
+  joint_element_x[0] = 0 + body_element_width[0]/2
+  joint_element_z[0] = 1.5
+  for i in range(number_body_elements-1):
+    body_element_x[i+1] = joint_element[0] + body_element_width[i+1]/2
+    if number_body_elements == 1:
+      break
+    if (i == (number_body_elements - 1)):
+      break
+    joint_element_x[i+1] = body_element_width[i+1]
   pyrosim.Start_URDF("body.urdf")
-  pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
-  pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
-  pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[1,1,1])
-  pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
-  pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
+  for i in range(number_body_elements-1):
+    pyrosim.Send_Cube(name=names_body_elements[i], pos=[body_element_x[i],body_element_y[i],body_element_z[i]], size=[body_element_width[i],body_element_length[i],body_element_height[i]])
+    name_new = names_body_elements[i] + "_" + names_body_elements[i+1]
+    pyrosim.Send_Joint(name = name_new , parent= names_body_elements[i] , child = names_body_elements[i+1] , type = "revolute", position = [joint_element_x[i],joint_element_y[i],joint_element_z[i]])
+  #pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
+  #pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
+  #pyrosim.Send_Cube(name="BackLeg", pos=[-0.5,0,-0.5], size=[1,1,1])
+  #pyrosim.Send_Joint(name = "Torso_FrontLeg" , parent= "Torso" , child = "FrontLeg" , type = "revolute", position = [2,0,1])
+  #pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
   pyrosim.End()
-def Generate_Brain():
+  Generate_Brain(number_body_elements,names_body_elements,body_element_width,body_element_length,body_element_height,touch_sensor_no_sensor)
+def Generate_Brain(number_body_elements,names_body_elements,body_element_width,body_element_length,body_element_height,touch_sensor_no_sensor):
   pyrosim.Start_NeuralNetwork("brain.nndf")
   pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
   pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
@@ -51,4 +85,4 @@ z = 0.5
 index = 0
 Create_World()
 Generate_Body()
-Generate_Brain()
+#Generate_Brain()
