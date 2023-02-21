d7593d534c40fb65501fa4a054518e180fd40826
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-20 22:11:44 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -35,6 +35,7 @@ class SOLUTION:
     self.limb_joint_element_x = []
     self.limb_joint_element_y = []
     self.limb_joint_element_z = []
+    self.limb_names = []
     for i in range(self.number_limbs):
       self.number_elements_per_limb[i] = random.randint(1,6) # simple number
       self.location_on_main_body_limb[i] = random.randint(0,self.body_num_el-1) # index
@@ -48,6 +49,7 @@ class SOLUTION:
       self.limb_joint_element_x.append( [0]*self.number_elements_per_limb[i] )
       self.limb_joint_element_y.append( [0]*self.number_elements_per_limb[i] )
       self.limb_joint_element_z.append( [0]*self.number_elements_per_limb[i] )
+      self.limb_names.append( [0]*self.number_elements_per_limb[i] )
       self.orientation_with_respect_to_first_limbs.append( [0]*self.number_elements_per_limb[i] )
       self.limb_sensors.append( [0]*self.number_elements_per_limb[i] )
       for j in range(self.number_elements_per_limb[i] ):
@@ -97,12 +99,15 @@ class SOLUTION:
       body_element_height[i] = round(random.uniform(0.1,2),2)
       #touch_sensor_no_sensor[i] = random.randint(0,1)
       names_body_elements[i] = str(i)
+    ii = number_body_elements
     for i in range(self.number_limbs):
       for j in range(self.number_elements_per_limb[i]):
         self.limb_dimensions_x[i].append(round(random.uniform(0.1,2),2))
         self.limb_dimensions_y[i].append(round(random.uniform(0.1,2),2))
         self.limb_dimensions_z[i].append(round(random.uniform(0.1,2),2))
         self.orientation_with_respect_to_first_limbs[i].append(random.randint(1,5))
+        self.limb_names[i][j] = str(ii)
+        ii = ii + 1
       self.orientation_with_respect_to_first_limbs[i][0] = 1
       self.limb_joint_element_x[i][0] = 0
       self.limb_joint_element_y[i][0] = 0
@@ -207,7 +212,23 @@ class SOLUTION:
             self.limb_joint_element_z[i][j+1] = -1*self.limb_dimensions_z[i][j]/2
             #body_element_x[i+1] = body_element_width[i+1]/2
             self.limb_positions_z[i][j+1] = -1*self.limb_dimensions_z[i][j+1]/2
-        
+    
+    joint_name_limb_list = []
+    for i in range(self.number_limbs):
+      for j in range(self.number_elements_per_limb[i]):
+        if self.limb_sensors[i][j]:
+          pyrosim.Send_Cube(name=self.limb_names[i][j], pos=[self.limb_positions_x[i][j],self.limb_positions_y[i][j],self.limb_positions_z[i][j]], size=[self.limb_dimensions_x[i][j],self.limb_dimensions_y[i][j],self.limb_dimensions_z[i][j]],COLOR_NAME="Green",RED="0.0",GREEN="1.0",BLUE="0.0")
+        else:
+          pyrosim.Send_Cube(name=self.limb_names[i][j], pos=[self.limb_positions_x[i][j],self.limb_positions_y[i][j],self.limb_positions_z[i][j]], size=[self.limb_dimensions_x[i][j],self.limb_dimensions_y[i][j],self.limb_dimensions_z[i][j]],COLOR_NAME="Blue",RED="0.0",GREEN="0.0",BLUE="1.0")
+        if (self.number_elements_per_limb[i] == 1):
+          break
+        if (j >= (self.number_elements_per_limb[i] - 1)): # add -1
+          break
+        name_new = self.limb_names[i][j] + "_" + self.limb_names[i][j+1]
+        joint_name_limb_list.append(name_new)
+        pyrosim.Send_Joint(name = name_new , parent= self.limb_names[i][j] , child = self.limb_names[i][j+1] , type = "revolute", position = [self.limb_joint_element_x[i][j],self.limb_joint_element_y[i][j],self.limb_joint_element_z[i][j]], jointAxis = "0 0 1")
+    
+    
     joint_name_list = []
     for i in range(number_body_elements):
       #print(touch_sensor_no_sensor)
@@ -229,6 +250,7 @@ class SOLUTION:
     #pyrosim.Send_Cube(name="FrontLeg", pos=[0.5,0,-0.5], size=[1,1,1])
     pyrosim.End()
     self.Create_Brain(joint_name_list,number_body_elements,names_body_elements,body_element_width,body_element_length,body_element_height,touch_sensor_no_sensor)
+    
   def Create_Brain(self,joint_name_list,number_body_elements,names_body_elements,body_element_width,body_element_length,body_element_height,touch_sensor_no_sensor):
     brain_file = "brain" + str(self.myID) + ".nndf"
     pyrosim.Start_NeuralNetwork(brain_file)
@@ -241,29 +263,15 @@ class SOLUTION:
     for element_name in joint_name_list:
       pyrosim.Send_Motor_Neuron( name = motor_name_index , jointName = element_name)
       motor_name_index = motor_name_index + 1
-    #pyrosim.Send_Sensor_Neuron(name = 0 , linkName = "Torso")
-    #pyrosim.Send_Sensor_Neuron(name = 1 , linkName = "BackLeg")
-    #pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
-    #pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
-    #pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
-    #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 3 , weight = -0.75 )
-    #pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -0.75 )
-    #pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = 1 )
-    #pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = 1 )
     sensor_neurons = list(range(sensor_name_index))
     motor_neurons = list(range(motor_name_index-sensor_name_index))
     for i in range(len(motor_neurons)):
       motor_neurons[i] = motor_neurons[i] + sensor_name_index
-    #for neuronName in self.nn.Get_Neuron_Names():
-    #print(sensor_neurons)
-    #print(motor_neurons)
-    #print(self.numSensor_Neurons)
-    #print(self.numMotor_Neurons)
     for currentRow in sensor_neurons:
       for currentColumn in motor_neurons:
         pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn, weight = self.weights[currentRow-1][currentColumn-sensor_name_index-1] )
     pyrosim.End()
-    #exit()
+    
   def Mutate(self):
     if self.numSensor_Neurons-1 == 0 or self.numSensor_Neurons-1 == -1:
       row_chosen = 0
