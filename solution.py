bd0c27356586470dd5df7e453497d4bd3d1667b4
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-20 22:30:59 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -235,8 +235,10 @@ class SOLUTION:
     for i in range(self.number_limbs):
       for j in range(self.number_elements_per_limb[i]):
         if self.limb_sensors[i][j]:
+          pass
           #pyrosim.Send_Cube(name=self.limb_names[i][j], pos=[self.limb_positions_x[i][j],self.limb_positions_y[i][j],self.limb_positions_z[i][j]], size=[self.limb_dimensions_x[i][j],self.limb_dimensions_y[i][j],self.limb_dimensions_z[i][j]],COLOR_NAME="Green",RED="0.0",GREEN="1.0",BLUE="0.0")
         else:
+          pass
           #pyrosim.Send_Cube(name=self.limb_names[i][j], pos=[self.limb_positions_x[i][j],self.limb_positions_y[i][j],self.limb_positions_z[i][j]], size=[self.limb_dimensions_x[i][j],self.limb_dimensions_y[i][j],self.limb_dimensions_z[i][j]],COLOR_NAME="Blue",RED="0.0",GREEN="0.0",BLUE="1.0")
         if (self.number_elements_per_limb[i] == 1):
           break
@@ -246,8 +248,10 @@ class SOLUTION:
         joint_name_limb_list.append(name_new)
         if j == 0:
           name_new = names_body_elements[self.location_on_main_body_limb[i]] + "_" + self.limb_names[i][j]
+          pass
           #pyrosim.Send_Joint(name = name_new , parent= names_body_elements[self.location_on_main_body_limb[i]] , child = self.limb_names[i][j] , type = "revolute", position = [self.limb_joint_element_x[i][j],self.limb_joint_element_y[i][j],self.limb_joint_element_z[i][j]], jointAxis = "0 0 1")
         else:
+          pass
           #pyrosim.Send_Joint(name = name_new , parent= self.limb_names[i][j] , child = self.limb_names[i][j+1] , type = "revolute", position = [self.limb_joint_element_x[i][j],self.limb_joint_element_y[i][j],self.limb_joint_element_z[i][j]], jointAxis = "0 0 1")
     #pyrosim.Send_Cube(name="Torso", pos=[1.5,0,1.5], size=[1,1,1])
     #pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1,0,1])
