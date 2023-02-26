4e2bbef0b5240e63145638e53feff1d2188584cd
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-25 23:51:27 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -128,11 +128,11 @@ class SOLUTION:
     #print("LOCATION ON MAIN BODY LIMB")
     #print(self.location_on_main_body_limb)
     body_element_x[0] = 0
-    body_element_z[0] = 5
+    body_element_z[0] = 10
     self.orientation_with_respect_to_first[0] = 1
     if number_body_elements != 1:
       joint_element_x[0] = 0 + body_element_width[0]/2
-      joint_element_z[0] = 5
+      joint_element_z[0] = 10
     for i in range(number_body_elements-1):
       if number_body_elements == 1:
         continue
