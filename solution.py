245a448cc592799192f0dccdeb156635523b5440
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-26 18:02:13 -0600

    Update solution.py

--- solution.py
+++ solution.py
@@ -365,7 +365,7 @@ class SOLUTION:
         self.limb_names[limb_selected].pop()
         if len(self.joint_name_limb_list[limb_selected]) != 0:
           self.joint_name_limb_list[limb_selected].pop()
-        if len(self.joint_name_limb_list[limb_selected]) != 0:
+        if len(self.joint_name_limb_list[limb_selected]) == 1:
           self.joint_name_limb_list[limb_selected].pop()
       self.number_elements_per_limb[limb_selected] = self.number_elements_per_limb[limb_selected] - 1
       if len(self.joint_name_limb_list[limb_selected]) != 0:
