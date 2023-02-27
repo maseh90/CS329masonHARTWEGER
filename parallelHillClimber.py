84990197bf85e7b2dd881455f8595c51f2e5ae9c
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-02-26 21:30:45 -0600

    Update parallelHillClimber.py

--- parallelHillClimber.py
+++ parallelHillClimber.py
@@ -77,10 +77,10 @@ class PARALLEL_HILL_CLIMBER:
   def Select(self):
     i = 0
     for key_parent in self.parents:
-      if self.parents[key_parent].fitness == float("NaN"):
+      if self.parents[key_parent].fitness == float('nan') and self.children[i] != float('nan'):
         self.parents[key_parent] = self.children[i]
         self.parents[key_parent].fitness = 1000
-      if (self.parents[key_parent].fitness > self.children[i].fitness):
+      elif (self.parents[key_parent].fitness > self.children[i].fitness):
         self.parents[key_parent] = self.children[i]
       i = i + 1
     #if (self.parent.fitness > self.child.fitness):
