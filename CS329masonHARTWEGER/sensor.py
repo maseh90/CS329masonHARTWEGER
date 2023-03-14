b086be91f99c6002c80eff355901dd8ed002f1f7
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-03-14 04:19:05 -0500

    Delete CS329masonHARTWEGER directory

--- CS329masonHARTWEGER/sensor.py
+++
@@ -1,7 +0,0 @@
-import pyrosim.pyrosim as pyrosim
-class SENSOR:
-  def __init__(self,inputName,data):
-    self.linkName = inputName
-    self.values = data
-  def Get_Value(self):
-    return pyrosim.Get_Touch_Sensor_Value_For_Link(self.linkName)
