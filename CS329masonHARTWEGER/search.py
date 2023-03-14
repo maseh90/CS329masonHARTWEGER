84cff3faf4af025f8fc09aa0a62a361fe8491d9e
maseh90 <79173890+maseh90@users.noreply.github.com>
2023-03-14 04:18:46 -0500

    Add files via upload

---
+++ CS329masonHARTWEGER/search.py
@@ -0,0 +1,11 @@
+import os
+from hillclimber import HILL_CLIMBER
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
+
+phc = PARALLEL_HILL_CLIMBER()
+#phc.Show_Best()
+phc.Evolve()
+phc.Show_Best()
+#for i in range(5):
+#  os.system("python3 generate.py")
+#  os.system("python3 simulate.py")
